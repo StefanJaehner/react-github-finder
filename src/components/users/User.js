8ee73cce2f1053ea8cfc433bd2ea593aee9697fd
Stefan Jaehner <31383343+StefanJaehner@users.noreply.github.com>
2021-03-04 15:14:14 +0100

    changed wording

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,76 +1,97 @@
-import React, { useEffect, Fragment, useContext } from 'react'
-import Spinner from '../layout/Spinner'
-import { Link } from 'react-router-dom'
-import Repos from '../repos/Repos'
-import GithubContext from '../../context/github/githubContext'
+import React, { useEffect, Fragment, useContext } from "react";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+import Repos from "../repos/Repos";
+import GithubContext from "../../context/github/githubContext";
 
-
-
-const User = ({match}) => {
-
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
-  const {user, loading, getUser, getUserRepos, repos } = githubContext;
+  const { user, loading, getUser, getUserRepos, repos } = githubContext;
 
-  useEffect( () => {
-    getUser(match.params.login)
-    getUserRepos(match.params.login)
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
   }, []);
 
-  const {login, name, avatar_url,
-    location, bio, blog, html_url,
-    followers, following, publich_repos,
-    public_gists,hireable, company, website
+  const {
+    login,
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    html_url,
+    followers,
+    following,
+    publich_repos,
+    public_gists,
+    hireable,
+    company,
+    website,
   } = user;
 
-  if (loading) return <Spinner/>;
+  if (loading) return <Spinner />;
 
   return (
     <Fragment>
       <Link to="/" className="btn btn-light">
         Back to search
       </Link>
-      Hireable: {' '}
-      {hireable ? <i className="fas fa-check text-success"/> :
-      <i className="fas fa-times-circle text-danger"/>}
+      Hireable:{" "}
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
       <div className="card grid-2">
         <div className="all-center">
-          <img 
-            src={avatar_url} 
-            alt="" 
+          <img
+            src={avatar_url}
+            alt=""
             className="round-img"
-            style={{width: '150px'}}
+            style={{ width: "150px" }}
           />
           <h1>{name}</h1>
           <p>Location: {location}</p>
         </div>
         <div>
-          {bio && <Fragment>
-            <h3>Bio</h3>
-            <p>{bio}</p>
-            </Fragment>}
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
           <a href={html_url} className="btn btn-dark my-1">
-            Visit Profile
+            Visit the Profile
           </a>
           <ul>
             <li>
-              {login && <Fragment>
-                <strong>Username: </strong> {login}
-                </Fragment>}
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
             </li>
             <li>
-              {company && <Fragment>
-                <strong>Company: </strong> {company}
-                </Fragment>}
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
             </li>
             <li>
-              {blog && <Fragment>
-                <strong>Blog: </strong> {blog}
-                </Fragment>}
+              {blog && (
+                <Fragment>
+                  <strong>Blog: </strong> {blog}
+                </Fragment>
+              )}
             </li>
             <li>
-              {website && <Fragment>
-                <strong>Website: </strong> {website}
-                </Fragment>}
+              {website && (
+                <Fragment>
+                  <strong>Website: </strong> {website}
+                </Fragment>
+              )}
             </li>
           </ul>
         </div>
@@ -81,9 +102,9 @@ const User = ({match}) => {
         <div className="badge badge-light">Public Repos: {publich_repos}</div>
         <div className="badge badge-dark">Public Gists: {public_gists}</div>
       </div>
-      <Repos repos={repos}/>
+      <Repos repos={repos} />
     </Fragment>
-  )
-}
+  );
+};
 
-export default User
+export default User;
